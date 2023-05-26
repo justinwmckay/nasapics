8040e06d5a32b88bce9033de8ca3cde7d5b0ed4f
Justin McKay <justinmckay99@gmail.com>
2023-05-26 12:03:33 -0700

    Initial Commit

---
+++ nasapics/nasapicsApp.swift
@@ -0,0 +1,17 @@
+//
+//  nasapicsApp.swift
+//  nasapics
+//
+//  Created by Justin McKay on 5/26/23.
+//
+
+import SwiftUI
+
+@main
+struct nasapicsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
